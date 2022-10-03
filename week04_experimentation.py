70463f50e58d87ab60a3464f0121a9470467ee9a
L1Gross <profgross@gmail.com>
2022-10-03 13:47:35 -0400

    Use insert, del, remove, sort, reverse, sorted, and loops

--- week04_experimentation.py
+++ week04_experimentation.py
@@ -23,3 +23,60 @@ print(mountains)
 # Print the length of the list.
 print(len(military))
 print(len(mountains))
+mountains.append('Mount Meru')
+print(mountains)
+print(mountains[1])
+# Remember to count from 0 rather than 1.
+mountains.insert(2, 'mount stanley'.title())
+print(mountains)
+# Use del to delete:
+del mountains[-1]
+print(mountains)
+# Remove an item by its value rather than its index:
+mountains.remove('Mount Stanley')
+print(mountains)
+# Pop the last element out of the list and use it.
+print(f"The second tallest mountain in Africa is {mountains.pop()}.")
+print(mountains)
+mountains.append('Mount Kenya')
+mountains.append('Mount Stanley')
+mountains.append('Mount Meru')
+print(mountains)
+print(f"The tallest mountain in Africa is {mountains.pop(0)}.")
+print(mountains)
+print(f"The second tallest mountain in Africa is {mountains.pop(0)}.")
+print(mountains)
+print(len(mountains))
+heights = [5199, 5109, 5895, 4562]
+print(heights)
+# The sort() method changes the order of a list permanently.
+heights.sort()
+print(heights)
+# The sorted() function returns a copy of the list,
+# leaving the original list unchanged.
+print(mountains)
+print(sorted(mountains))
+print(mountains)
+print(military)
+print(sorted(military))
+print(military)
+print(sorted(military, reverse = True))
+print(military)
+
+print('These are the branches:')
+for branch in military:
+    print('One branch is:')
+    print(branch)
+print('Those are all the branches.')
+
+print('These are the branches:')
+for i in range(len(military)):
+    print('One branch is:')
+    print(military[i])
+print('Those are all the branches.')
+
+
+
+
+
+
